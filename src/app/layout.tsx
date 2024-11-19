17a37a008efd7972d6f032a6d897faf5d9e185b3
kumeyuta <ku8.m.1.e2@gmail.com>
2024-11-20 01:13:06 +0900

    metaタグ設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,24 @@ export const metadata: Metadata = {
   title: 'Relumo（リルモ）',
   description:
     'Relumo（リルモ）へようこそ。Relumoは、招待された人だけが参加できるプライベートな掲示板アプリ。仕事の話や何気ない雑談も、気ままにシェア。もっと自由に、もっと開放的に。安心して心を開けるプライベートな空間を提供します。',
+  openGraph: {
+    url: 'https://relumo.vercel.app',
+    type: 'website',
+    images: '/ogp.jpg',
+    locale: 'ja_JP',
+    siteName: 'Relumo（リルモ）',
+  },
+  twitter: {
+    site: '@saga_engineer',
+  },
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/manifest.webmanifest',
 };
 
 export default function RootLayout({
