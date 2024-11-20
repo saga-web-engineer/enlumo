df0704e528d9fe8143a36e1e214fbecc253246c6
kumeyuta <ku8.m.1.e2@gmail.com>
2024-11-20 10:50:40 +0900

    既存クラスがありpropsで追加のクラスを渡すときはcnを使用するように変更

--- src/app/components/Wrapper.tsx
+++ src/app/components/Wrapper.tsx
@@ -1,10 +1,12 @@
 import type { FC } from 'react';
 
+import { cn } from '@/lib/utils';
+
 interface Props {
   children: React.ReactNode;
   className?: string;
 }
 
 export const Wrapper: FC<Props> = ({ children, className }) => {
-  return <div className={`mx-auto w-[min(90%,1200px)] ${className}`}>{children}</div>;
+  return <div className={cn('mx-auto w-[min(90%,1200px)]', className)}>{children}</div>;
 };
